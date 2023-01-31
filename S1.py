f8c584e99912bc5b27a89e4572f43272b024a83b
Antonio <toni.psk@mail.ru>
2023-01-31 23:25:49 +0400

    commit 1 Try

--- S1.py
+++ S1.py
@@ -1,10 +1,23 @@
-# n = int(input())
-# m = int(input())
+# klass1 = int(input('Введите количество учеников в 1 классе '))
+# klass2 = int(input())
+# klass3 = int(input())
+# part1 = klass1 // 2
+# part2 = klass2 // 2
+# part3 = klass3 // 2
+# result = part1 + part2 + part3
+# # print(f'количество парт для всех классов будет {result}')
+# klass1 = int(input('Введите количество учеников в 1 классе '))
+# part1 = klass1 // 2 if klass1 % 2 == 0 else klass1 // 2 + 1
+# print(part1)
 
-n = 700
-m = 21000
-if m % n == 0:
-    result = m // n 
-else: 
-    result = m // n + 1
-print(f'что бы проехать на машине {m} км, нужно ехать {result} дн')
+year = int(input('Введите год '))
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    print('yes')
+else:
+    print('no')
+
+print('Yes' if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0 else 'No')
+
+condition1 = year % 4 == 0 and year % 100 != 0
+condition2 = year % 400 == 0
+print('Yes' if condition1 or condition2 else 'No')
